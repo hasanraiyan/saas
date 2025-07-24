cfc73d022a3e88cd1f6ef84016d20055bc4b6b2c
Raiyan Hasan <raiyanhasan2006@gmail.com>
2025-07-24 17:25:17 +0530

    NavBar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
 
-
 const Page = () => {
   return (
     <div>
@@ -9,6 +8,7 @@ const Page = () => {
       <h1 className="text-2xl underline">
             Welcome to my SaaS app!
           <br />
+
             <Button>
                 Let&apos;s get started!
             </Button>
